bba339164a2d0fd16f9c4055d57fa89aeb39d9ae
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-06-08 22:06:24 +0530

    Add files via upload

---
+++ tutorial3.py
@@ -0,0 +1,23 @@
+country = "United States"
+year_founded = 1776
+
+
+user_age = 20 # valid
+
+website = 'https://instagram.com' # valid
+
+# 1password = True # invalid
+# DATATYPES - INTEGER, FLOAT, STRING, CHARACTER, BOOLEAN, 
+
+answer = 42
+weight = 34.592
+message = "Hello Python"
+is_authenticated = True
+
+
+print(type(answer))  # Output: <class 'int'>
+print(type(message))  # Output: <class 'str'>
+
+
+
+
